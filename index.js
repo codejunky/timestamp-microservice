8fac02b5d72729cbbe7f52fbf13203f6df66e0eb
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-22 21:10:19 +0100

    Take the date passed by the user and give back a unix and natural formatted date

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var moment = require('moment');
+
+var app = express();
+
+app.get('/:date', function(req, res) {
+
+  var dateParam = req.params.date;
+  var human = null;
+  var unix = null;
+
+  if (dateParam.match(/^[0-9]+$/)) {
+    unix = dateParam;
+    human = moment.unix(dateParam).format('MMMM DD, YYYY');
+  } else if (dateParam.match(/\b\w+\b [0-9]{2}, [0-9]{4}/)) {
+      human = dateParam
+      unix = moment(dateParam, 'MMMM DD, YYYY').unix();
+  }
+
+
+  res.json({
+    unix: unix,
+    natural: human
+  });
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('App listening on 3000');
+});
